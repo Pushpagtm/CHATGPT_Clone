0d512554c9423d2ec39d6c5f60512ee88290e6c7
Pushpagtm <pushpagautam529@gmail.com>
2023-05-24 21:11:53 +0545

    component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
 import OptionSelection from './Components/OptionSelection';
-
+import Translation from './Components/Translation';
+import { arrayItems } from './AIOptions';
+import { useState } from 'react';
 function App() {
+  
+  const [option, setOption] = useState({});
+  const [result, setResult] = useState("");
+  const [input, setInput] = useState("");
+  const selectOption = (option) => {
+    setOption(option);
+  };
+  const doStuff = async () => {
+    let object = { ...option, prompt: input };
+
+    // const response = await openai.createCompletion(object);
+
+    // setResult(response.data.choices[0].text);
+  };
   return (
-   <OptionSelection/>
+    <div className='App'>
+       {Object.values(option).length === 0 ? (
+      <OptionSelection arrayItems={arrayItems} selectOption={selectOption} />
+    ) : (
+      <Translation doStuff={doStuff} setInput={setInput} result={result} />
+    )}
+
+    </div>
+   
   );
 }
 
--- src/Components/OptionSelection.js
+++ src/Components/OptionSelection.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
-function OptionSelection(props) {
+function OptionSelection({arrayItems,selectOption}) {
     return (
         <div>
-            <h1>Select Option</h1>
+           <h1 className="heading">ChatGPT Clone</h1>
+           <div className="grid-main">
+        {arrayItems.map((item) => {
+          return (
+            <div
+              className="grid-child"
+              onClick={() => selectOption(item.option)}
+            >
+              <h3>{item.name}</h3>
+              <p>{item.description}</p>
+            </div>
+          );
+        })}
+      </div>
         </div>
     );
 }
